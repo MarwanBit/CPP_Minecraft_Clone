71754d15cca059f6cbb0d2f5b6bda0e8720db8db
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2023-03-26 22:59:05 -0700

    Working on Implementing Textures and Abstracting Buffer Objects

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -10,83 +10,11 @@
 #include "Renderer.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "VertexBufferLayout.h"
+#include "Shader.h"
+#include "VertexArray.h"
+#include "Texture.h"
 
-
-struct ShaderProgramSource {
-    std::string VertexSource;
-    std::string FragmentSource;
-};
-
-static ShaderProgramSource ParseShader(const std::string& filepath) {
-    std::ifstream stream(filepath);
-
-    enum class ShaderType {
-        NONE = -1, VERTEX = 0, FRAGMENT = 1
-    };
-    std::string line;
-    std::stringstream ss[2];
-    ShaderType type = ShaderType::NONE;
-    while (getline(stream, line)) {
-        if (line.find("#shader") != std::string::npos) {
-            if (line.find("vertex") != std::string::npos) {
-                // set mode to vertex
-                type = ShaderType::VERTEX;
-            }
-            else if (line.find("fragment") != std::string::npos) {
-                // set mode to fragment
-                type = ShaderType::FRAGMENT;
-            }
-        
-        } else {
-            ss[(int)type] << line << "\n";
-        }
-    }
-    return { ss[0].str(), ss[1].str() };
-}
-
-static unsigned int CompileShader(unsigned int type, const std::string& source) {
-    unsigned int id = glCreateShader(type);
-    const char* src = source.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
-
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-    if (result == GL_FALSE) {
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* message = (char*) _malloca(length * sizeof(char));
-        glGetShaderInfoLog(id, length, &length, message);
-        std::cout << "Failed to compile" << 
-            (type == GL_VERTEX_SHADER ? "vertex" : "fragment") 
-            << "!" << std::endl;
-        std::cout << message << std::endl;
-        glDeleteShader(id);
-        return 0;
-    }
-
-
-    return id;
-}
-
-static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
-    unsigned int program = glCreateProgram();
-    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-
-    /*Attach the vs, and fs to the program and link it to compile on the GPU*/
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    glDetachShader(program, vs);
-    glDetachShader(program, fs);
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-
-    return program;
-}
 
 int main(void)
 {
@@ -123,10 +51,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     float positions[] = {
-        -0.5f, -0.5f, //0
-        0.5f,   -0.5f, //1
-        0.5f,  0.5f, //2
-        -0.5f, 0.5f, //3
+        -0.5f, -0.5f, 0.0f, 0.0f, //0
+        0.5f,   -0.5f, 1.0f, 0.0f, //1
+        0.5f,  0.5f, 1.0f, 1.0f, //2
+        -0.5f, 0.5f, 0.0f, 1.0f //3
     };
 
     /* create an index buffer which tells you which order
@@ -142,31 +70,41 @@ int main(void)
     GLCall(glGenVertexArrays(1, &vao));
     GLCall(glBindVertexArray(vao));
 
+    VertexArray va;
     /*Now we are going to create a buffer for our triangle*/
-    VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+    VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+
+    VertexBufferLayout layout;
+    layout.Push<float>(2);
+    layout.Push<float>(2);
+    va.AddBuffer(vb, layout);
+
     GLCall(glEnableVertexAttribArray(0));
     GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0));
 
     /* create the index buffer object, bind it and send to gpu*/
     IndexBuffer ib(indices, 6);
 
-    
-    ShaderProgramSource source = ParseShader("res\\shaders\\Basic.shader");
-    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    GLCall(glUseProgram(shader));
+    /*Create a shader object which we will use to handle shaders and uniforms*/
+    Shader shader{ "res\\shaders\\Basic.shader" };
+    shader.Bind();
 
     /*use a uniform to send variables into shaders*/
-    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
-    ASSERT(location != -1);
-    GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+    shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+
+    Texture texture("res/textures/Grass_Block.png");
+    texture.Bind();
+    shader.SetUniform1i("u_Texture", 0);
 
 
     /*Unbind buffers for the point of demonstration*/
-    GLCall(glBindVertexArray(0));
-    GLCall(glUseProgram(0));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+    ib.UnBind();
+    shader.UnBind();
+    vb.UnBind();
+    va.UnBind();
 
+    Renderer renderer;
 
     /*Time to write shaders!!! */ 
     float r = 0.0f;
@@ -177,17 +115,17 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        GLCall(glClear(GL_COLOR_BUFFER_BIT));
+        renderer.Clear();
 
         /*Bind Buffers on each call*/
-        GLCall(glUseProgram(shader));
-        GLCall(glBindVertexArray(vao));
+        shader.Bind();
+        va.Bind();
         ib.Bind();
    
         /*Issue a draw call for our buffer*/
         /*Uniforms are used per draw call, uniforms are set per draw*/
-        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
-        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+        shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+        renderer.Draw(va, ib, shader);
 
         if (r > 1.0f) {
             increment = -0.05f;
@@ -205,7 +143,6 @@ int main(void)
         GLCall(glfwPollEvents());
     }
 
-    GLCall(glDeleteProgram(shader));
     glfwTerminate();
     return 0;
-}
+};
--- OpenGL/src/Renderer.h
+++ OpenGL/src/Renderer.h
@@ -2,6 +2,10 @@
 
 #include <GL/glew.h>
 
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
+
 #define ASSERT(x) if (!(x)) __debugbreak();
 #define GLCall(x) GLClearError();\
     x;\
@@ -10,4 +14,19 @@
 
 void GLClearError(); 
 
+
 bool GLLogCall(const char* function, const char* file, int line); 
+
+
+class Renderer {
+
+public:
+    Renderer();
+    ~Renderer();
+    void Clear() const;
+    void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+
+private:
+
+
+};
